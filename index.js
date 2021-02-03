77a2b8257a304bc2984801b9c0cbecba78d6d43a
gracenak <gracenak@gmail.com>
2021-02-02 18:30:25 -0700

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 function returnFirstTwoDrivers(array) {
     return array.slice(0, 2)
-
 }
 
 function returnLastTwoDrivers(array) {
     return array.slice(-2)
-
 }
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
